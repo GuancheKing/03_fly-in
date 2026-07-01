cc7fc871df2f7d7168deb34ab1a09c7b13bdcd85
Pepo <pepojimenezbethencourt@gmail.com>
2026-07-01 16:56:06 +0000

    simulation.py v1 ready

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from src.parsing.parser import MapParser
-from src.pathfinding.pathfiner import PathFinder
+from src.simulation.simulation import Simulation
 
 
 def main() -> None:
     parser = MapParser("/home/pepo/projects/repos/42/03/03_fly_in/maps/easy/01_linear_path.txt")
+
     graph = parser.parse()
-    finder = PathFinder(graph)
-    print(finder.path_exists())
-    print(finder.find_path())
+
+    simulation = Simulation(graph)
 
 
 if __name__ == "__main__":
--- src/simulation/simulation.py
+++ src/simulation/simulation.py
@@ -6,13 +6,28 @@ from src.pathfinding.pathfiner import PathFinder
 class Simulation:
     def __init__(self, graph: Graph):
         self.graph = graph
-        self.path = PathFinder(self.graph)
+        self.pathfinder = PathFinder(self.graph)
         self.turn = 0
         self.drones = []
         self._create_drones()
+        self._prepare_paths()
 
     def _create_drones(self):
+        # Create all drones at the start zone.
         for drone in range(1, self.graph.nb_drones + 1):
             id = f"D{drone}"
             starting_zone = self.graph.start_zone
             self.drones.append(Drone(id, starting_zone))
+
+    def _prepare_paths(self):
+        if not self.pathfinder.path_exists():
+            raise RuntimeError("No path found")
+
+        for drone in self.drones:
+            # Assign an initial path to every drone.
+            drone_path_names = self.pathfinder.find_path()
+
+            # Convert zone names into Zone objects.
+            for zone in drone_path_names:
+                drone.path.append(self.graph.zones[zone])
+            print(drone.id, [zone.name for zone in drone.path])
