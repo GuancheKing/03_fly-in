8a8ebea4c80af06e9884897c7fa55e3aa55f16c7
Pepo <pepojimenezbethencourt@gmail.com>
2026-07-09 07:44:57 +0000

    feat: replace manual command line input parsing with argpars

--- main.py
+++ main.py
@@ -1,25 +1,52 @@
-import sys
+import argparse
 from src.parsing.parser import MapParser, MapError
 from src.simulation.simulation import Simulation, SimulationError
 from src.pathfinding.pathfiner import PathError
 
 
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Run the fly-in drone simulation."
+    )
+
+    parser.add_argument(
+        "map_file",
+        help="Path to the map file to simulate."
+    )
+
+    parser.add_argument(
+        "--visual",
+        action="store_true",
+        help="Display a visual terminal representation each turn."
+    )
+
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        help="Display simulation statistics after execution"
+    )
+
+    return parser.parse_args()
+
+
 def main() -> None:
-    if len(sys.argv) != 2:
-        print("Usage: python main.py <map_file>")
-        return
 
-    filename = sys.argv[1]
-
+    args = parse_args()
     try:
-        graph = MapParser(filename).parse()
-        simulation = Simulation(graph, debug=True, visual=True)
+        graph = MapParser(args.map_file).parse()
+        simulation = Simulation(
+            graph,
+            debug=args.debug,
+            visual=args.visual
+            )
         simulation.run()
+
     except (MapError, PathError, SimulationError) as error:
         print(error)
         return
+
     except FileNotFoundError:
-        print(f"File not found: {filename}")
+        print(f"File not found: {args.map_file}")
         return
 
 
